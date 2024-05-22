548a0723eb003421fb47d71b6f4347c4ecae4b34
joaowozh <joaoleopoldowozhiak@gmail.com>
2024-05-22 13:34:22 -0300

    Aprendendo layout
    Adicionei:
    - Título
    - Texto
    - Botão
    - Tabela

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 import streamlit as st
+import pandas as pd
 
 def main():
-    # a simple title
-    st.title("Hello World!")
+    # titulo
+    st.title("Opa!")
 
-    # a simple text
-    st.text("This is a simple text.")
+    # descrição
+    st.text('''     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et dui turpis.
+    Nulla eu dolor ac magna iaculis tempus. Mauris quis metus tortor.
+    Pellentesque ac pulvinar lectus, vel blandit erat.''')
 
-    # a simple markdown
-    st.markdown("### This is a simple markdown.")
+    # botão
+    if st.button("Carregar dados"):
+        st.title("Dados carregados!")
+    
+    # exemplos pra tabela
+    data = {
+    'Nome': ['Alice', 'Pablo', 'Carlos'],
+    'Idade': [24, 27, 15]
+}
 
-    # a simple button
-    st.button("Simple Button")
+    # converter para dataframe
+    df = pd.DataFrame(data)
+
+    # mostrando tabela
+    st.table(df)
 
 if __name__ == "__main__":
     main()
