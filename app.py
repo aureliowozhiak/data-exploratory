60f5a6c404b1b667e8cb79e5f23fc3c28b19003b
joaowozh <joaoleopoldowozhiak@gmail.com>
2024-05-23 16:02:47 -0300

    Novos checkbox
    Adicionei novos checkbox

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ def main():
     st.text('''     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et dui turpis.
     Nulla eu dolor ac magna iaculis tempus. Mauris quis metus tortor.
     Pellentesque ac pulvinar lectus, vel blandit erat.''')
-
+    sep = st.text_input('Digite qual vai ser o separador: ')
     uploaded_file = st.file_uploader("Carregue um arquivo CSV", type="csv")
 
     if uploaded_file is not None:
         try:
-            df = pd.read_csv(uploaded_file)
+            df = pd.read_csv(uploaded_file, sep = sep)
             st.write("Arquivo carregado com sucesso!")
 
             # Ver primeiras linhas (usando a função head)
@@ -21,18 +21,35 @@ def main():
 
             # Ver últimas linhas
             # Crie um checkbox para mostrar as últimas linhas (dica use a função tail)
-            #
-            #
-            #
-
+            if st.checkbox("Mostrar últimas linhas"):
+                st.write(df.tail())
+            
             # Ver tudo
             if st.checkbox("Mostrar tudo"):
                 st.write(df)
 
             # Ver colunas
             # Crie um checkbox para mostrar as colunas do dataframe (dica use a função columns)
-            #
-            #
+            if st.checkbox('Mostrar colunas'):
+                st.write(df.columns)
+
+            # Pesquisar outras funções para vizualização de tabela (describe)
+            # Ver uma linha aleátoria
+            if st.checkbox("Mostrar linha aleátoria"):
+                st.write(df.sample())
+            
+            # Número de linhas e número de colunas
+            if st.checkbox("Número de linhas e colunas"):
+                st.write(df.shape)
+
+            # Estatistícas númericas das colunas
+            if st.checkbox("Mostrar números"):
+                st.write(df.describe())
+
+            # Exibir o tipo de dados de cada coluna
+            if st.checkbox("Tipo de dados"):
+                st.write(df.dtypes)
+
 
         except Exception as e:
             st.write("Erro ao ler arquivo CSV")
